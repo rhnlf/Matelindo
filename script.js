546a32dbda21479c2c17909943dd0037ef9873e5
rhnlf_ <alif.satoe@gmail.com>
2024-07-25 11:25:52 +0700

    Change number

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function sendWA() {
     let phone = document.getElementById('phone');
     let address = document.getElementById('address');
     let message = document.getElementById('message');
-    
+
     if (name.value.trim() == "") {
         name.style.background = "lightpink";
         name.style.border = "4px solid red";
@@ -12,7 +12,7 @@ function sendWA() {
         return false;
     }
 
-    let url = "https://wa.me/6285157114685?text="
+    let url = "https://wa.me/6285156262472?text="
         + "Nama: " + name.value + "%0a"
         + "Email: " + email.value + "%0a"
         + "Nomor Telepon: " + phone.value + "%0a"
