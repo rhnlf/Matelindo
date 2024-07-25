08d875fc13b800dc7887f53a41039d5e83a2d19e
rhnlf_ <alif.satoe@gmail.com>
2024-07-25 11:23:30 +0700

    Update form submission to use WhatsApp integration for better user experience

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ function sendWA() {
     let phone = document.getElementById('phone');
     let address = document.getElementById('address');
     let message = document.getElementById('message');
-    let mobileNumber = 6285157114685;    //Enter your mobile number here
-
+    
     if (name.value.trim() == "") {
         name.style.background = "lightpink";
         name.style.border = "4px solid red";
@@ -13,7 +12,12 @@ function sendWA() {
         return false;
     }
 
-    let url = `https://wa.me/${mobileNumber}?text=` + "Nama: " + name.value + "%0a" + "Email: " + email.value + "%0a" + "Nomor Telepon: " + phone.value + "%0a" + "Alamat: " + address.value + "%0a" +  "Pesan: " + message.value;
+    let url = "https://wa.me/6285157114685?text="
+        + "Nama: " + name.value + "%0a"
+        + "Email: " + email.value + "%0a"
+        + "Nomor Telepon: " + phone.value + "%0a"
+        + "Alamat: " + address.value + "%0a"
+        + "Pesan: " + message.value;
 
     window.open(url, '_blank').focus();
 }
